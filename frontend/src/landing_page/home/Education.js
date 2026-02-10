eb4ef667b8a86eade9e15579508447d56d524644
kamal <shivamkamal.9936@gmail.com>
2026-02-10 21:16:49 +0530

    Fixed frontend folder issue

---
+++ frontend/src/landing_page/home/Education.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Education() {
+    return ( 
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col'>
+                    <img src='media/images/education.svg' style={{width:"70%"}}/>
+
+                </div>
+                <div className='col'>
+                    <h1 className='mb-3 fs-2'>Free and open market education</h1>
+                    <p>varsity the largestt online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <a href='' className='mr-5'style={{textDecoration:"none"}}>Versity<i class="fa fa-long-arrow-right" aria-hidden="ture"></i></a>
+                    <p className='mt-5'>TradingQ&A, the most active trading and investment community in market related queries. </p>
+                    <a href='' className='mr-5'style={{textDecoration:"none"}}>TradingQ&A<i class="fa fa-long-arrow-right" aria-hidden="ture"></i></a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Education;
