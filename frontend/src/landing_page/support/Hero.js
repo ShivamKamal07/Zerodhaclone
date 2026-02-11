7e3ca1dd01012d1e582f152694cde090e3af137d
kamal <shivamkamal.9936@gmail.com>
2026-02-11 20:49:22 +0530

    issue resolve

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
@@ -14,19 +15,19 @@ function Hero() {
           </h1>
           <input placeholder="Eg: how do I activate F&O, why is my order getting rejected " />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <a href="#">Track account opening</a>
+          <a href="#">Track segment activation</a>
+          <a href="#">Intraday margins</a>
+          <a href="#">Kite user manual</a>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+              <a href="#">Current Takeovers and Delisting - January 2024</a>
             </li>
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <a href="#">Latest Intraday leverages - MIS & CO</a>
             </li>
           </ol>
         </div>
