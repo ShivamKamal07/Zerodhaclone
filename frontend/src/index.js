fe38d7085f65258a4112645f10388c1df6d468bc
kamal <shivamkamal.9936@gmail.com>
2026-02-13 17:42:07 +0530

    login and signup page done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./index.css";
+
 import Navbar from "./landing_page/Navbar";
 import HomePage from "./landing_page/home/HomePage";
-import Signup from "./landing_page/signup/Signup";
+import Signup from "./landing_page/pages/Signup";
+import Login from "./landing_page/pages/Login";
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -12,19 +14,40 @@ import SupportPage from "./landing_page/support/SupportPage";
 import Footer from "./landing_page/Footer";
 import NotFound from "./landing_page/NotFound";
 
+function Layout() {
+  const location = useLocation();
+
+  // yaha decide kar rahe hain kab hide karna hai
+  const hideLayout =
+    location.pathname === "/signup" ||
+    location.pathname === "/login";
+
+  return (
+    <>
+      {!hideLayout && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  </React.StrictMode>
 );
